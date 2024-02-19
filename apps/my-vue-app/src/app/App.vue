52bc6ee949f731478383e2efc44613c9cc3173e3
Katerina Skroumpelou <sk.katherine@gmail.com>
2024-02-19 17:16:20 +0200

    added vue app

---
+++ apps/my-vue-app/src/app/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import NxWelcome from './NxWelcome.vue'
+import { One } from "@my-nuxt-workspace/my-vue-lib-1";
+</script>
+
+<template>
+  <NxWelcome title="my-vue-app" />
+  <One />
+</template>
